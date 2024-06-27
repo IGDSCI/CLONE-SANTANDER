4b4c62d75293ab8fdd57253c78b785616e20b0f3
Gustavo dos Santos Capeletti <gustavocapeletti007@gmail.com>
2024-06-27 16:43:45 -0300

    site completo

---
+++ js.js
@@ -0,0 +1,7 @@
+function mascaraCPF(cpf) {
+    cpf.value = cpf.value.replace(/\D/g, '')
+                        .replace(/(\d{3})(\d)/, '$1.$2')
+                        .replace(/(\d{3})(\d)/, '$1.$2')
+                        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+                        .replace(/(-\d{2})\d+?$/, '$1');
+}
