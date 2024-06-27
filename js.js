8fea3bdf13f6405270f6bb0b50b48675aabffdc3
Gustavo dos Santos Capeletti <gustavocapeletti007@gmail.com>
2024-06-27 16:51:24 -0300

    Delete js.js

--- js.js
+++
@@ -1,7 +0,0 @@
-function mascaraCPF(cpf) {
-    cpf.value = cpf.value.replace(/\D/g, '')
-                        .replace(/(\d{3})(\d)/, '$1.$2')
-                        .replace(/(\d{3})(\d)/, '$1.$2')
-                        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
-                        .replace(/(-\d{2})\d+?$/, '$1');
-}
